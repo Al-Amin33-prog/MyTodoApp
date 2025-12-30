bba73f1b7143dcf33fc135f931cec067e2258ab1
AL-Amin <aminushaba33@gmail.com>
2025-12-30 08:28:21 +0100

    Initial commit: Base version of Todo App
    This commit includes the foundational structure of the Todo App with core functionalities implemented. Future updates will include enhancements such as database integration, improved UI/UX,
    Polish UI: animations, checkbox feedback, and layout fixes
    update  on  navigation structure
    Add app screenshots for documents
    updated text field content
    updated  uis
    added a login screen
    login screen static
    separated ui concerns
    updated  icons
    adjusted  navigation compose
    introduced room
    updated dataclass to taskEntity(Table)
    updated state
    moved entity files into a package
    added a local database
    introduced dao
    updated my state structure
    introduced database
    separated concerns, ->introduced database provider
    added a static login screen, firebase coming soon
    updated my add edit screen
    added a splash logo
    updated repository structure to fit room dao
    integrated all project files to viewmodel
    introduced viewModel factory
    some changes to navigator
    updated ui
    updated add edit screen

--- app/src/main/java/com/example/myapplication1/Task1.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication1
-
-data class TaskModel(
-    val id: Int = 0,
-    val title: String,
-    val description: String = "",
-    val isDone: Boolean = false
-)
--- app/src/main/java/com/example/myapplication1/Task4.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication1
-
- sealed class TaskState {
-     object TaskLoading: TaskState()
-     object TaskEmpty: TaskState()
-     data class TaskSuccess(val taskModel: List<TaskModel>): TaskState()
-     data class TaskError(val message: String): TaskState()
-}

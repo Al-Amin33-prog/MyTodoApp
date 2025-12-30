26c4b793c58c9702f340e93b1750f8862b0b27c5
AL-Amin <aminushaba33@gmail.com>
2025-12-30 08:20:17 +0100

    Initial commit: Base version of Todo App
    This commit includes the foundational structure of the Todo App with core functionalities implemented. Future updates will include enhancements such as database integration, improved UI/UX, updated my navigation structure
    Polish UI: animations, checkbox feedback, and layout fixes
    Add app screenshots for documents
    updated text field content
    updated my uis
    added a login screen
    login screen static
    separated ui concerns
    updated my icons
    adjusted  navigation compose
    introduced room
    updated dataclass to taskEntity(Table)
    moved entity files into a package
    added a local database
    introduced dao
    updated my state structure
    introduced database
    separated concerns, ->introduced database provider
    added a static login screen, firebase coming soon
    updated my add edit screen

---
+++ app/src/main/java/com/example/myapplication1/data/local/dao/TaskDao.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication1.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.myapplication1.data.local.entity.TaskEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao{
+    @Query("SELECT * FROM tasks ORDER BY createdAt DESC")
+    fun getAllTasks(): Flow<List<TaskEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: TaskEntity)
+
+    @Delete
+    suspend fun deleteTask(task: TaskEntity)
+
+    @Query("SELECT * FROM tasks WHERE id = :taskId")
+    suspend fun getTaskById(taskId:Int): TaskEntity?
+
+    @Update
+    suspend fun updateTask(task: TaskEntity)
+
+    @Query("DELETE FROM tasks")
+    suspend fun deleteAll()
+
+}
